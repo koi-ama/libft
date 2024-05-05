a554ae5ee546a39cce554b9d86be3eb4d68c5d88
koi-ama-m3p <koiama@amakasukiichirounoMacBook-Pro.local>
2024-05-05 21:56:08 +0900

    all-libft-including-BONUS-but-atoi-OVERFLOW

---
+++ ft_lstadd_back.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kamakasu <kamakasu@student.42.jp>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/05 17:31:38 by kamakasu          #+#    #+#             */
+/*   Updated: 2024/05/05 21:09:02 by kamakasu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+#include "libft.h"
+
+void	ft_lstadd_back(t_list **lst, t_list *new)
+{
+	t_list *last;
+
+	if (lst && new)
+	{
+		if (*lst == NULL)
+			*lst = new;
+		else
+		{
+			last = ft_lstlast(*lst);
+			last->next = new;
+		}
+	}
+}
